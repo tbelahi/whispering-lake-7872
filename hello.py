290df6759ed3eaae6ca5ad4063cbba158e6b91b6
tbelahi <belahi@ipgp.fr>
2013-10-14 22:33:21 +0200

    la carte de paris est finie

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 
 import os
+import re
 from flask import Flask, render_template_string, render_template, url_for, redirect
 import json
 import urllib2
@@ -20,7 +21,7 @@ def raph():
 
 @app.route('/ipgp')
 def ipgp():
-  # clé pour API JCDecaux 
+  # clé pour API JCDecaux
   api_key='d6177aa449272d6c0bdde000927553cf45ac7c50'
 
   # stations around IPGP:
@@ -34,7 +35,7 @@ def ipgp():
     response = urllib2.urlopen(url).read()
     # put them in a dictionnary
     station_real_time = json.loads(response)
-    
+
     #create the marker corresponding to the station and it's status
     status = station_real_time['status']
     name = station_real_time['name'].encode('ascii','replace')
@@ -42,7 +43,7 @@ def ipgp():
     available_bike_stands = station_real_time['available_bike_stands']
     lon = station_real_time['position']['lng']
     lat = station_real_time['position']['lat']
-    s += '<p>{0} status: {1}, bikes: {2} ,free spots: {3}</p>\n'.format(name,\
+    s += '<p>{0} status: {1}, available bikes: {2}, free parking spots: {3}</p>\n'.format(name,\
         status, available_bikes, available_bike_stands)
 
   #return 'en construction'
@@ -55,7 +56,7 @@ def paris():
   url = 'https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=d6177aa449272d6c0bdde000927553cf45ac7c50'
   response = urllib2.urlopen(url).read()
   stations = json.loads(response)
-  
+
   map_paris = folium.Map([48.85329,2.34902], zoom_start=12)
   # fetching and plotting info on map
   for station in stations:
@@ -66,7 +67,7 @@ def paris():
     available_bike_stands = station['available_bike_stands']
     lon = station['position']['lng']
     lat = station['position']['lat']
-    s = '{0} status: {1}, bikes: {2} ,free spots: {3}'.format(name,\
+    s = '{0} status: {1}, bikes: {2}, free parking spots: {3}'.format(name,\
            status, available_bikes, available_bike_stands)
     s = s.encode('ascii', 'replace')
     if available_bikes==0:
@@ -79,8 +80,7 @@ def paris():
 
   map_paris.create_map('paris.html')
   with open('paris.html') as f:
-    s = '{%extends "layout.html"%}\n{%block body%}\n'+f.read()+'\n{% endblock %}'
-  with open('templates/paris.html', 'w') as g:
-    g.write(s)
+    s = f.read()
+  ss = '{%extends "layout.html"%}\n{%block body%}\n'+re.search('<!DOCTYPE html>.*</head>(.*)',s,re.DOTALL|re.MULTILINE).group(1)+'\n{% endblock %}'
   #return render_template('paris.html')
-  return render_template_string(s)
+  return render_template_string(ss)
