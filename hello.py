0f6952a41b98171e3f4f2e7b8a166217c009f3c6
tbelahi <belahi@ipgp.fr>
2013-10-21 14:48:18 +0200

    si je code comme ça tout le temps, ça ne va pas le faire

--- hello.py
+++ hello.py
@@ -13,7 +13,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db.SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 
 @app.route('/')
